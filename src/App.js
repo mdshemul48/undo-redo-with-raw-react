3f696a11330dc6a2356ff3e43c98eaee9b7a8788
MD. Shimul <mdshemul480@gmail.com>
2021-11-30 00:40:54 +0600

    undo redo done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [content, setContent] = useState('hello world');
+  const [track, setTrack] = useState(0);
+  const [tempState, setTempState] = useState([]);
+  const [undoRedoStatus, setUndoRedoStatus] = useState({
+    undoDisabled: true,
+    redoDisabled: true,
+  });
+
+  // first time store content to tempState
+  useEffect(() => {
+    setContent(content);
+    setTempState([content]);
+    setUndoRedoStatus({
+      undoDisabled: true,
+      redoDisabled: true,
+    });
+  }, []);
+
+  // changing undo redo states
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+    setUndoRedoStatus({
+      undoDisabled: false,
+      redoDisabled: true,
+    });
+  };
+  console.log('=====================');
+  console.log(content, tempState, undoRedoStatus, track);
+  console.log('=====================');
+
+  const onBlur = (event) => {
+    const value = event.target.value;
+    if (content === tempState[tempState.length - 1]) return;
+    setTempState([...tempState, value]);
+    setTrack(tempState.length);
+    console.log('gg', tempState.length);
+  };
+
+  const redo = () => {
+    if (track === tempState.length) return;
+    setTrack(track + 1);
+    setContent(tempState[track + 1]);
+    setUndoRedoStatus((prevState) => ({
+      ...prevState,
+      undoDisabled: false,
+    }));
+
+    if (track === tempState.length - 2) {
+      setUndoRedoStatus((prevState) => ({
+        ...prevState,
+        redoDisabled: true,
+      }));
+    }
+  };
+  const undo = () => {
+    if (track === 0) {
+      return;
+    }
+    setTrack(track - 1);
+    console.log('from this com', undoRedoStatus);
+    if (0 === track - 1) {
+      setUndoRedoStatus({
+        ...undoRedoStatus,
+        undoDisabled: true,
+        redoDisabled: false,
+      });
+    } else {
+      setUndoRedoStatus((prevState) => ({
+        ...prevState,
+        redoDisabled: false,
+      }));
+    }
+
+    setContent(tempState[track - 1]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={undo} disabled={undoRedoStatus.undoDisabled}>
+        undo
+      </button>
+      <button onClick={redo} disabled={undoRedoStatus.redoDisabled}>
+        redo
+      </button>
+      <br />
+      <textarea
+        value={content}
+        onChange={handleChange}
+        onBlur={onBlur}
+        id='w3review'
+        name='w3review'
+        rows='4'
+        cols='50'
+      ></textarea>
     </div>
   );
 }
